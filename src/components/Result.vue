7ddb2de729c624eabd1f73fffb7b71fde22952f7
madortill <mador.till@gmail.com>
2025-07-07 17:37:21 +0300

    see pics

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="result">
     <p class="headline">{{ arrRoles[resultIndex].name }}</p>
-    <img class="role-img" :src="arrRoles[resultIndex].imgUrl" alt="roleImg" />
+    <img class="role-img" :src="getRoleSrc()" alt="roleImg" />
     <img class="soldier" src="/media/soldier/finish.png" alt="soldier"/>
   </div>
 </template>
@@ -16,32 +16,42 @@ export default {
       arrRoles: [
         {
           name: "מפקד/ת נוער",
-          imgUrl: "/media/role/role0.jpeg",
+          imgUrl: "media/role/role0.jpeg",
         },
         {
           name: 'מפק"צ חינוך',
-          imgUrl: "/media/role/role1.jpeg",
+          imgUrl: "media/role/role1.jpeg",
         },
         {
           name: 'מש"ק/ית חו"יה',
-          imgUrl: "/media/role/role2.jpeg",
+          imgUrl: "media/role/role2.jpeg",
         },
         {
           name: 'מש"ק/ית הוראה והדרכה',
-          imgUrl: "/media/role/role3.jpeg",
+          imgUrl: "media/role/role3.jpeg",
         },
         {
           name: 'מש"ק/ית גשרים',
-          imgUrl: "/media/role/role4.jpeg",
+          imgUrl: "media/role/role4.jpeg",
         },
         {
           name: 'מש"ק/ית תקומה',
-          imgUrl: "/media/role/role5.jpeg",
+          imgUrl: "media/role/role5.jpeg",
         },
       ],
     };
   },
-  methods: {},
+  computed: {
+    baseURL() {
+    // Use Vite's built-in import.meta.env to get the base URL
+    return import.meta.env.BASE_URL || "/";
+  }
+  },
+  methods: {
+    getRoleSrc() {
+      // Dynamically adjust the src path based on the base URL
+      return `${this.baseURL}${this.arrRoles[this.resultIndex].imgUrl}`;
+    },},
 };
 </script>
 
