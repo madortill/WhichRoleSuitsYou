bfff15e4875161c55ba7894bedb8fb4dcaad4e8f
madortill <mador.till@gmail.com>
2025-07-09 10:47:53 +0300

    fine result

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -6,10 +6,20 @@
       alt="restart"
       class="restart"
     />
-<p class="fix-text top-text">איזה כיף {{ userName }}!</p>
-<p class="fix-text">קיבלת:</p>
-    <p class="headline">{{ arrRoles[resultIndex].name }}</p>
-    <img class="role-img" :src="getRoleSrc()" alt="roleImg" />
+    <p class="top-text">איזה כיף {{ userName }}!</p>
+    <div class="headline-container">
+      <p class="mini-text">קיבלת &nbsp;</p>
+      <p class="headline">{{ arrRoles[resultIndex].name }}</p>
+    </div>
+
+    <p class="text-guide">לעוד פרטים על התפקיד- הדוכן פה ממול</p>
+
+    <div class="img-container">
+      <img src="/media/grayLace.png" alt="lace" class="gray-lace" />
+      <img class="role-img" :src="getRoleSrc()" alt="roleImg" />
+      <div class="fix-lace"></div>
+    </div>
+
     <img class="soldier" src="/media/soldier/finish.png" alt="soldier" />
   </div>
 </template>
@@ -75,29 +85,59 @@ export default {
   flex-direction: column;
   align-items: center;
   justify-content: flex-start;
-  color: #157145;
-  font-size: 2rem ;
+  color: #2180c3;
+  font-size: 2rem;
   font-weight: bold;
 }
 
 .headline {
- 
+  font-size: 3rem;
+}
+
+.headline-container {
   font-size: 2.5rem;
-  margin-top: 5rem;
-  color: #2180c3;
+  margin-top: 0rem;
+  display: flex;
+  align-items: baseline; 
+  width: 100vw;
+  justify-content: center;
+}
+
+.mini-text {
+  font-size: 1.5rem;
+}
+
+.img-container {
+  max-width: 95%;
+  position: relative;
 }
 
 .role-img {
   border-radius: 2rem;
-    max-height: 23rem;
-    max-width: 95%;
+  max-height: 23rem;
+  max-width: 95%;
+}
+
+.gray-lace {
+  position: absolute;
+  height: 100%;
+  width: 7rem;
+}
+
+.fix-lace {
+  height: 3rem;
+}
+
+.text-guide {
+  margin-top: -2rem;
+  font-size: 1.5rem;
 }
 
 .soldier {
   width: 20rem;
-    position: absolute;
-    bottom: -12rem;
-    left: 0rem;
+  position: absolute;
+  bottom: -12rem;
+  left: 0rem;
 }
 
 .restart {
@@ -108,12 +148,10 @@ export default {
   cursor: pointer;
 }
 
-.fix-text {
-  margin: 0rem;
-}
-
 .top-text {
+  margin: 0rem;
   margin-top: 6rem;
+  color: #157145;
 }
 @media screen and (max-width: 500px) {
   #result {
